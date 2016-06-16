08332b4583edd9351073808df247c1e9b70533eb
Mariko Wakabayashi <marimarimekko@gmail.com>
2016-06-16 18:58:11 -0500

    working on login process...

--- app/scripts/eventoZukuri.js
+++ app/scripts/eventoZukuri.js
@@ -1,3 +1,12 @@
 'use strict';
 
-angular.module('eventoZukuri', ['firebase']);
+angular.module('eventoZukuri', ['firebase', 'ui.router'])
+	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
+		$stateProvider
+			.state('login', {
+				url: '/',
+				templateUrl: 'login/login-view.html',
+				controller: 'login/login-ctrl.js'
+			});
+	}]);
--- app/scripts/login/login-ctrl.js
+++ app/scripts/login/login-ctrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('eventoZukuri', ['firebase'])
-    .controller('MainCtrl', ['$scope', '$firebaseAuth', function ($scope, $firebase) {
+    .controller('LoginCtrl', ['$scope', '$firebaseAuth', function ($scope, $firebase) {
         var ref = new Firebase('https://eventozukuri.firebaseio.com/');
         var auth = $firebaseAuth(ref);
 
