798cc38489a4c8756dfa25b34548c2d2c16672d8
Mariko Wakabayashi <marimarimekko@gmail.com>
2016-06-28 18:07:09 -0500

    console success and error messages when signing in for testing purposes

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,10 +7,13 @@ angular
 		this.data = $firebaseObject(ref);
 		
 		this.signIn = function() {
-			firebase.auth().signInWithEmailAndPassword(this.email, this.password).catch(function(error) {
+			firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(function(user) {
+				console.log("Signed in as: ", user.uid);
+			}).catch(function(error) {
 				// Handle Errors here.
 				var errorCode = error.code;
 				var errorMessage = error.message;
+				console.log("Authentication Failed: ", errorMessage);
 			});
 		};
 	}]);
