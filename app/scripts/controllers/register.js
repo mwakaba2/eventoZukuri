5ae6ecfb34d5f9595651c05f99f26cf6d60251a6
Mariko Wakabayashi <marimarimekko@gmail.com>
2016-06-28 19:03:34 -0500

    Saving user info to database during registration

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -6,12 +6,30 @@ angular
 		var ref = firebase.database().ref();
 		this.data = $firebaseObject(ref);
 
-		this.signUp = function () {
-			firebase.auth().createUserWithEmailAndPassword(this.email, this.password).catch(function(error) {
+		this.signUp = function (name, email, password, employer, jobTitle, bday) {
+			firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
+				console.log("Creating user: ", user.uid);
+				saveUser(user.uid, name, email, employer, jobTitle, bday);
+			}).catch(function(error) {
 				// Handle Errors here.
 				var errorCode = error.code;
 				var errorMessage = error.message;
+				console.log("Signing up failed: ", errorMessage);
 			});
 		};
+
+		function saveUser(userId, name, email, employer, jobTitle, bday) {
+			employer = employer === undefined ? '' : employer;
+			jobTitle = jobTitle === undefined ? '' : jobTitle;
+			bday = bday === undefined ? '' : bday;
+			
+			firebase.database().ref('users/' + userId).set({
+				name: name,
+				email: email,
+				employer: employer,
+				jobTitle: jobTitle,
+				bday: bday
+			});
+		}
 
 	}]);
