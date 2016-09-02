c704c4c270aae259e404d1a29d53033021450d9c
Mariko Wakabayashi <marimarimekko@gmail.com>
2016-09-01 22:06:15 -0500

    Cleaning up code for pwvalid

--- app/scripts/directives/pwvalid.js
+++ app/scripts/directives/pwvalid.js
@@ -1,18 +1,17 @@
-'use strict';
-
 angular
   .module('eventoZukuri')
   .directive('pwValid', function () {
-	return {
-		require: 'ngModel',
-		link: function (scope, elem, attrs, ctrl) {
-			elem.on('keyup', function () {
-				scope.$apply(function () {
-					var regExpString = new RegExp('((?=.*\\d)(?=.*[A-Z])(?=.*\\W).{8,})');
-					var v = regExpString.test(elem.val());
-					ctrl.$setValidity('pwinvalid', v);
+		'use strict';
+		return {
+			require: 'ngModel',
+			link: function (scope, elem, attrs, ctrl) {
+				elem.on('keyup', function () {
+					scope.$apply(function () {
+						var regExpString = new RegExp('((?=.*\\d)(?=.*[A-Z])(?=.*\\W).{8,})');
+						var v = regExpString.test(elem.val());
+						ctrl.$setValidity('pwinvalid', v);
+					});
 				});
-			});
-		}
-	};
+			}
+		};
 });
